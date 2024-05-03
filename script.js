ee5040c6d717fba769a44c5311e72400152ccc1d
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-03 14:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function findLargest(a, b, c) {
   //your code here
+	if (a>b)
+// 	{
+// 		if(a>c) return a;
+// 		else return c;
+// 	}
+// 	if(b>a)
+// 	{
+// 		if(b>c) return b;
+// 		else return c;
+// 	}
+// 	if(c>a)
+// 	{
+// 		if(c>b) return c;
+// 		else return b;
+// 	}
+// }
+function findLargest(a, b, c) {
+  //your code here
+	if(a>b && a>c)
+		return a
+	else if(b>a && b>c)
+		return b
+	else
+		return c
+}
+
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
